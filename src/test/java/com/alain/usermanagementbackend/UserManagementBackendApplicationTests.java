7b5d4e0abaf44665387c1b38c8668cebd3f4b989
Alain Mignot <alainmignot.tech@gmail.com>
2025-12-02 14:24:24 -0700

    Add Gradle project files

---
+++ src/test/java/com/alain/usermanagementbackend/UserManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alain.usermanagementbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
