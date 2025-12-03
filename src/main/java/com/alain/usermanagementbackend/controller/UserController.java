80906319a4439f1958ee07cbd78f5ca9cfc32080
Alain Mignot <alainmignot.tech@gmail.com>
2025-12-03 01:32:40 -0700

    Support PUT requests using @PutMapping

--- src/main/java/com/alain/usermanagementbackend/controller/UserController.java
+++ src/main/java/com/alain/usermanagementbackend/controller/UserController.java
@@ -34,4 +34,15 @@ public class UserController {
         return userRepository.findById(id)
                 .orElseThrow(() -> new UserNotFoundException(id));
     }
+
+    @PutMapping("/{id}")
+    User updateUser(@PathVariable Long id, @RequestBody User newUser) {
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setUsername(newUser.getUsername());
+                    user.setName(newUser.getName());
+                    user.setEmail(newUser.getEmail());
+                    return userRepository.save(user);
+                }).orElseThrow(() -> new UserNotFoundException(id));
+    }
 }
