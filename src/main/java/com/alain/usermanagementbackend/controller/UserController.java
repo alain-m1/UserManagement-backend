8719658475e58ddfae1cd29b8eab3ea712886bb9
Alain Mignot <alainmignot.tech@gmail.com>
2025-12-02 18:32:36 -0700

    Create the UserController and map GET/POST for users endpoint

---
+++ src/main/java/com/alain/usermanagementbackend/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.alain.usermanagementbackend.controller;
+
+import com.alain.usermanagementbackend.model.User;
+import com.alain.usermanagementbackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User newUser) {
+        return userRepository.save(newUser);
+    }
+}
