a2ec8e6ee276759d7713b98d5d567073cbcb1f91
Alain Mignot <alainmignot.tech@gmail.com>
2025-12-03 01:16:50 -0700

    Include Cross-Origin, create custom exception handling

--- src/main/java/com/alain/usermanagementbackend/controller/UserController.java
+++ src/main/java/com/alain/usermanagementbackend/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.alain.usermanagementbackend.controller;
 
+import com.alain.usermanagementbackend.exception.UserNotFoundException;
 import com.alain.usermanagementbackend.model.User;
 import com.alain.usermanagementbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 @RequestMapping("/users")
 public class UserController {
 
@@ -25,5 +27,11 @@ public class UserController {
     @PostMapping
     public User createUser(@RequestBody User newUser) {
         return userRepository.save(newUser);
+    }
+
+    @GetMapping("/{id}")
+    User getUserById(@PathVariable Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException(id));
     }
 }
